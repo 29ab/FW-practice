7cf74b68b10f2d1ffa42780e8332c63c2bcc0b41
29ab <87421954+29ab@users.noreply.github.com>
2022-06-08 19:34:17 +0530

    updated carousel and image gallery

--- Sumathy_assignment/image_gallery.js
+++ Sumathy_assignment/image_gallery.js
@@ -1,12 +1,19 @@
-let Images = JSON.parse(localStorage.getItem('images'));
+// let Images = JSON.parse(localStorage.getItem('images'));
+
+let Images = [
+    "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/168938/pexels-photo-168938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ];
     
 let main_view = document.getElementById("gallery-view");
 
-let div = document.createElement("div"); // div holds the and display all the images from array together
-div.setAttribute('class','gallery-image');
+let div1 = document.createElement("div"); // div holds the and display all the images from array together
+div1.setAttribute('id','gallery-image');
 
-let display_image = document.createElement("div"); // div to hold and display images in bigger size. 
-display_image.setAttribute('class','display-big-image');
+let display_image = document.createElement("div"); // (Right Side) div to hold and display images in bigger size. 
+display_image.setAttribute('class','display-right-side');
 
 let default_image = document.createElement("img"); // to show default 1st image in the array
 default_image.src = Images[0];
@@ -16,24 +23,61 @@ Images.forEach(function (el) {
     
     let image = document.createElement("img");
     image.src = el;
-    div.append(image);
-
-    image.addEventListener('click',function(){
-        showImage(el);
-    });
-
-    image.addEventListener('mouseover',function(){
-        showImage(el);
-    })
-   
+    image.setAttribute('class','thumbnails');
+    div1.append(image);
 });
 
-function showImage(ele){
-    console.log('Element:=',ele)
+
+
+main_view.append(div1);
+
+let thumbnails = document.getElementById('gallery-image');
+thumbnails.addEventListener('click', function(e) {
+
+  if(e.target.classList.contains('thumbnails')){
+    let source_link = e.target.src;
+    showImage(source_link);
+  }
+});
+
+function showImage(source_link){
+    
     display_image.innerHTML = " ";
+    
     let image1 = document.createElement("img");
-    image1.src = ele;
-    display_image.append(image1)
+    image1.src = source_link;
+    display_image.append(image1);
 }
 
-main_view.append(div,display_image);
+main_view.append(display_image);
+
+// display_image.addEventListener('mouseover',  startSlideshow);
+// // display_image.addEventListener('mouseout', function(){
+// //   document.querySelector("img").innerHTML = " ";
+// // })
+
+// function startSlideshow() {
+  
+//     let counter = 0;
+
+//     let interval = setInterval(function () {
+
+//       // container.innerText = 
+//       let div = document.createElement("div");
+//       div.setAttribute("class", "w3-animate-right")
+
+//       if (counter === Images.length) {
+//         counter = 0;
+//       }
+
+//       let img = document.createElement("img");
+//       img.src = Images[counter];
+
+//     div.append(img)
+//     display_image.append(div)
+
+//       counter = counter + 1;
+//     }, 2000);
+//   }
+
+
