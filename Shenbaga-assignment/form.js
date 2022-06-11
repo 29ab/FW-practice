e17f8e75a5a374276e98669b4d30cede83e2fad5
29ab <87421954+29ab@users.noreply.github.com>
2022-06-11 16:35:47 +0530

    updated the code of debouncing

--- Shenbaga-assignment/form.js
+++ Shenbaga-assignment/form.js
@@ -1,51 +1,149 @@
 
 let Show_result = document.getElementById('display-information');
 
-function inputFirstName() {
+let Employee_FName = document.querySelector('#first-name');
+let dbounce_Employee_Fname = document.getElementById('fname');
 
-    let name = document.querySelector('#first-name').value;
-    document.getElementById('fname').innerHTML = name;
-    Employee_first_name = name;
+const UpdatedVlaue = () => {
+    dbounce_Employee_Fname.innerHTML = Employee_FName.value;
 }
 
-function inputLastName() {
-    let lname = document.getElementById('last-name').value;
-    document.getElementById('lname').innerHTML = lname;
+let debounceTimer;
+const debounce = (callback,time) => {
+    window.clearTimeout(debounceTimer);
+    debounceTimer = window.setTimeout(callback,time)
+} 
+
+Employee_FName.addEventListener('input',()=> {
+    debounce(UpdatedVlaue,200);
+},false);
+
+
+let Employee_lname = document.getElementById('last-name');
+let debounceLname = document.getElementById('lname');
+
+const UpdatedVlaueLname = () => {
+    debounceLname.innerHTML = Employee_lname.value;
 }
 
-function inputEmail() {
-    let email = document.getElementById('Email').value;
-    document.getElementById('display-email').innerHTML = email;
+let debounceTimerLName;
+const debounceforLName = (callback,time) => {
+    window.clearTimeout(debounceTimerLName);
+    debounceTimerLName = window.setTimeout(callback,time)
+} 
+
+Employee_lname.addEventListener('input',()=> {
+    debounceforLName(UpdatedVlaueLname,200);
+},false);
+
+
+let Employee_Email = document.getElementById('Email');
+let debouncedEmail = document.getElementById('display-email');
+
+Employee_Email.addEventListener('input', () => {
+    debounceForEmail(updateEmail,200);
+},false)
+
+let debounceTimerEmail;
+const debounceForEmail = (callback,time) => {
+    window.clearTimeout(debounceTimerEmail);
+    debounceTimerEmail = window.setTimeout(callback,time);
 }
 
-function inputAddress() {
-    let address = document.getElementById('permanent-address').value;
-    document.getElementById('display-address').innerHTML = address;
-}
-
-function inputStatusS() {
-    let status = document.getElementById('Single-status').value;
-    document.getElementById('display-maritalStatus').innerHTML = status;
-}
-
-function inputStatusM() {
-    let status = document.getElementById('Married-status').value;
-    document.getElementById('display-maritalStatus').innerHTML = status;
-}
-
-function inputStatusD() {
-    let status = document.getElementById('Divorced-status').value;
-    document.getElementById('display-maritalStatus').innerHTML = status;
-}
-
-function inputDegree() {
-    let degree = document.getElementById('degree').value;
-    document.getElementById('display-degree').innerHTML = degree;
+const updateEmail = () => {
+    debouncedEmail.innerHTML = Employee_Email.value;
 }
 
 
-document.getElementById('Submit-button').addEventListener('click', function () {
+let Employee_Address = document.getElementById('permanent-address');
+let debouncedAdderss = document.getElementById('display-address');
 
+Employee_Address.addEventListener('input',()=>{
+    debounceforAddress(UpdatedAddress,200);
+},false)
+
+let debounceAddressTimer;
+const debounceforAddress = (callback,time) =>{
+    window.clearTimeout(debounceAddressTimer);
+    debounceAddressTimer = window.setTimeout(callback,time);
+}
+
+const UpdatedAddress = () => {
+    debouncedAdderss.innerHTML = Employee_Address.value;
+}
+
+let Employee_Single = document.getElementById('Single-status');
+let debouncedSingle = document.getElementById('display-maritalStatus');
+
+Employee_Single.addEventListener('input',()=>{
+    debouncedforSingleStatus(UpdatedSingleStatus,200);
+},false);
+
+let debounceSingleStatusTimer;
+const debouncedforSingleStatus = (callback,time) => {
+    window.clearTimeout(debounceSingleStatusTimer);
+    debounceSingleStatusTimer = window.setTimeout(callback,time);
+}
+
+const UpdatedSingleStatus = () =>{
+    debouncedSingle.innerHTML = Employee_Single.value;
+};
+
+let Employee_Married = document.getElementById('Married-status');
+let debouncedMarried = document.getElementById('display-maritalStatus');
+
+Employee_Married.addEventListener('input',()=>{
+    debouncedforMarriedStatus(UpdatedMarriedStatus,200);
+},false);
+
+let debounceMarriedStatusTimer;
+const debouncedforMarriedStatus = (callback,time) => {
+    window.clearTimeout(debounceMarriedStatusTimer);
+    debounceMarriedStatusTimer = window.setTimeout(callback,time);
+}
+
+const UpdatedMarriedStatus = () =>{
+    debouncedMarried.innerHTML = Employee_Married.value;
+};
+
+
+let Employee_Divorced = document.getElementById('Divorced-status');
+let debouncedDivorced = document.getElementById('display-maritalStatus');
+
+Employee_Divorced.addEventListener('input',()=>{
+    debouncedforDivorcedStatus(UpdatedDivorcedStatus,200);
+},false);
+
+let debounceDivorcedStatusTimer;
+const debouncedforDivorcedStatus = (callback,time) => {
+    window.clearTimeout(debounceDivorcedStatusTimer);
+    debounceDivorcedStatusTimer = window.setTimeout(callback,time);
+}
+
+const UpdatedDivorcedStatus = () =>{
+    debouncedDivorced.innerHTML = Employee_Divorced.value;
+};
+
+let Employee_Degree = document.getElementById('degree');
+
+let debouncedDegree = document.getElementById('display-degree');
+
+Employee_Degree.addEventListener('change',()=> {
+    console.log("change",Employee_Degree.value)
+    debouncedforDegree(UpdatedDegree,200);
+},false)
+
+let debouncedDegreeTimer;
+let debouncedforDegree = (callback,time)=> {
+    window.clearTimeout(debouncedDegreeTimer);
+    debouncedDegreeTimer = window.setTimeout(callback,time); 
+}
+
+const UpdatedDegree = ()=> {
+    debouncedDegree.innerHTML = Employee_Degree.value;
+}
+
+document.getElementById('Submit-button').addEventListener('click', function(){
     let Employee_first_name = document.getElementById('fname').innerHTML;
     let Employee_last_name = document.getElementById('lname').innerHTML;;
     let Employee_email = document.getElementById('display-email').innerHTML;;
@@ -63,7 +161,7 @@ document.getElementById('Submit-button').addEventListener('click', function () {
     }
 
     localStorage.setItem("employee_information", JSON.stringify(employee_details));
-    
+
     document.querySelector('#first-name').value = " "
     document.getElementById('last-name').value = " ";
     document.getElementById('Email').value = " ";
@@ -73,7 +171,5 @@ document.getElementById('Submit-button').addEventListener('click', function () {
     document.getElementById('Divorced-status').checked = false;
     document.getElementById('degree').value = " ";
 
-    Show_result.innerHTML = " ";
-
     window.location.href = "viewform.html";
-});
+})
